350b36ec32fac3c13fecc70d11c8e57ad68dd5c3
machado-leandro <69988365+machado-leandro@users.noreply.github.com>
2020-08-28 14:54:54 -0300

    Add files via upload

--- teste.cpp
+++ teste.cpp
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 
 int main(){
-	//vetor de 12 posiÃ§Ãµes
+	//vetor de 12 posições
 	int v[12], i, cont,cont1,cont2,aux[12];
 	float porcentagem,porcentagem1,porcentagem2;
 	
@@ -19,19 +19,23 @@ int main(){
 	for(i=0; i<12;i++)//*
 		if (v[i] <=20 && v[i]>21 || v[i]<=30 && v[i]<=31 || v[i]>=50)
 		cont = cont + 1;
-
+		//if (v[i]>=21 || v[i]<=30)
+		//if (v[i]<=31 || v[i]>=50);
+			//cont = cont +1;
 
 
 	
 	//calculo da porcentagem
 	porcentagem = (cont*i)/100;//*
+	porcentagem1 = (cont*i)/100;
+	porcentagem2 = (cont*i)/100;
 
 	
 	printf("\n\t vetor ",&v[i]);
 	//saida dos valores
 	printf("\n\t Porcentagem dentro do range de 1..20: %.2f \n",porcentagem);//*
-	printf("\n\t Porcentagem dentro do range de 22..30: %.2f \n",porcentagem);
-	printf("\n\t Procentagem dentro do range de 31..50: %.2f \n", porcentagem);
+	printf("\n\t Porcentagem dentro do range de 22..30: %.2f \n",porcentagem1);
+	printf("\n\t Procentagem dentro do range de 31..50: %.2f \n", porcentagem2);
 		
 	return 0;
 	system("pause");
